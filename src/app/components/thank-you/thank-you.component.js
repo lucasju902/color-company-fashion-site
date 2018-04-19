ef16b12f98675590c29ebf6ab3ce3b30783baad0
Sergey Kryuchkov <sergeykr101@gmail.com>
2018-04-19 21:16:24 +0300

    thanks page

---
+++ src/app/components/thank-you/thank-you.component.js
@@ -0,0 +1,8 @@
+angular
+    .module('app')
+    .component('thankYouComponent', {
+      templateUrl: 'app/components/thank-you/thank-you.tmpl.html',
+      controller: function () {
+        this.fr = 'idk';
+      }
+    });
