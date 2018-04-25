f9930afe80f91dabf6bb6f51effbcc3181e74058
Sergey Kryuchkov <sergeykr101@gmail.com>
2018-04-25 15:18:21 +0300

    updates server

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(cors());
 app.set('port', (process.env.PORT || 5000));
 
-var distDir = __dirname + "/dist/";
+var distDir = __dirname + '/dist/';
 app.use(express.static(distDir));
 
 app.listen(app.get('port'), function () {
@@ -15,53 +15,52 @@ app.listen(app.get('port'), function () {
 
 app.get('/api', function (req, res) {
   request.get('http://hueauto.herokuapp.com/api/main', function (err, resp, body) {
-    if (err) res.status(500).json(err)
+    if (err) res.status(500).json(err);
     var response = body;
     try {
-      response = JSON.parse(response)
+      response = JSON.parse(response);
     }
     catch (err) {
     }
-    res.json(response)
-  })
+    res.json(response);
+  });
 });
 
 app.get('/api/login', function (req, res) {
   request.get('http://www.hue-group.com/api/login.jsonp', {qs: req.query}, function (err, resp, body) {
-    if (err) res.status(500).json(err)
+    if (err) res.status(500).json(err);
     var response = body;
     try {
-      response = JSON.parse(response)
+      response = JSON.parse(response);
     }
     catch (err) {
     }
-    res.json(response)
-  })
+    res.json(response);
+  });
 });
 
 app.get('/api/logout', function (req, res) {
   request.get('http://www.hue-group.com/api/logout.jsonp', {qs: req.query}, function (err, resp, body) {
-    if (err) res.status(500).json(err)
+    if (err) res.status(500).json(err);
     var response = body;
     try {
-      response = JSON.parse(response)
+      response = JSON.parse(response);
     }
     catch (err) {
     }
-    res.json(response)
-  })
+    res.json(response);
+  });
 });
 
 app.get('/api/user', function (req, res) {
   request.get('http://www.hue-group.com/api/user.jsonp', {qs: req.query}, function (err, resp, body) {
-    if (err) res.status(500).json(err)
+    if (err) res.status(500).json(err);
     var response = body;
     try {
-      response = JSON.parse(response)
+      response = JSON.parse(response);
     }
     catch (err) {
     }
-    res.json(response)
-  })
+    res.json(response);
+  });
 });
-
