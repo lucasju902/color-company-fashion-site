d73bf7f2254e8436f6f57095d05f306c56738603
Sergey Kryuchkov <sergeykr101@gmail.com>
2018-06-07 18:17:28 +0300

    style and bug fix

--- src/app/components/profile/profile.component.js
+++ src/app/components/profile/profile.component.js
@@ -13,38 +13,43 @@ angular
 
       vm.init = function () {
         authService.loadCurrentUser().then(function (res) {
-          $http.get(appConfig.dashboardServiceUrl + 'members/' + res.data.user.id + '.json')
+          vm.userID = res.data.user.id
+          $http.get(appConfig.dashboardServiceUrl + 'members/' + vm.userID + '.json')
             .then(function (res) {
               if (res && res.data) {
                 vm.userData = res.data;
                 vm.date = vm.userData.date_day + ' : ' + vm.userData.date_month + ' : ' + vm.userData.date_year;
                 vm.img_url = 'http://s3.amazonaws.com/hue-storage/huegroup-website/about_vertical_coverages/image1s/000/000/001/original/HG_REPORT-V1-image.001.jpeg?1523632810';
 
-                var jobIndex = vm.searchIndex(vm.jobs, vm.userData.job_function);
-                var comSizeIndex = vm.searchIndex(vm.companySizes, vm.userData.company_size);
-                var countryIndex = vm.searchIndex(vm.countries, vm.userData.country);
-                var industryIndex = vm.searchIndex(vm.industries, vm.userData.industry);
+                vm.jobIndex = vm.searchIndex(vm.jobs, vm.userData.job_function);
+                vm.comSizeIndex = vm.searchIndex(vm.companySizes, vm.userData.company_size);
+                vm.countryIndex = vm.searchIndex(vm.countries, vm.userData.country);
+                vm.industryIndex = vm.searchIndex(vm.industries, vm.userData.industry);
 
-                vm.data = {
-                  first_name: {value: vm.userData.first_name, required: true, name: 'first name', type: 'provide'},
-                  last_name: {value: vm.userData.last_name, required: true, name: 'last name', type: 'provide'},
-                  email: {value: vm.userData.email, name: 'email', type: 'provide'},
-                  company: {value: vm.userData.company, required: true, name: 'company name', type: 'provide'},
-                  job_title: {value: vm.userData.job_title, required: true, name: 'job title', type: 'provide'},
-                  bio: {value: vm.userData.bio, name: 'bio', type: 'provide'},
-                  job_function: {value: vm.jobs[jobIndex], required: true, name: 'job function', type: 'select'},
-                  company_size: {
-                    value: vm.companySizes[comSizeIndex],
-                    required: true,
-                    name: 'company size',
-                    type: 'select'
-                  },
-                  industry: {value: vm.industries[industryIndex], required: true, name: 'industry', type: 'select'},
-                  country: {value: vm.countries[countryIndex], required: true, name: 'country', type: 'select'}
-                };
+                vm.intEditData();
               }
             });
         });
+      };
+
+      vm.intEditData = function () {
+        vm.data = {
+          first_name: {value: vm.userData.first_name, required: true, name: 'first name', type: 'provide'},
+          last_name: {value: vm.userData.last_name, required: true, name: 'last name', type: 'provide'},
+          email: {value: vm.userData.email, name: 'email', type: 'provide'},
+          company: {value: vm.userData.company, required: true, name: 'company name', type: 'provide'},
+          job_title: {value: vm.userData.job_title, required: true, name: 'job title', type: 'provide'},
+          bio: {value: vm.userData.bio, name: 'bio', type: 'provide'},
+          job_function: {value: vm.jobs[vm.jobIndex], required: true, name: 'job function', type: 'select'},
+          company_size: {
+            value: vm.companySizes[vm.comSizeIndex],
+            required: true,
+            name: 'company size',
+            type: 'select'
+          },
+          industry: {value: vm.industries[vm.industryIndex], required: true, name: 'industry', type: 'select'},
+          country: {value: vm.countries[vm.countryIndex], required: true, name: 'country', type: 'select'}
+        };
       };
 
       vm.searchIndex = function (arr, value) {
@@ -60,6 +65,7 @@ angular
 
       vm.cancel = function () {
         vm.editFlag = false;
+        vm.intEditData();
       };
 
       vm.save = function () {
@@ -73,7 +79,7 @@ angular
             }
           }
           data.flag = 'profile';
-          $http.put(appConfig.dashboardServiceUrl + 'members/' + $stateParams.id, data)
+          $http.put(appConfig.dashboardServiceUrl + 'members/' + vm.userID, data)
             .then(function (res) {
             if (res.status !== 200) {
               console.log(res);
