ba1783ab0e0dc7a00ad157a48045094cc8273110
Sergey Kryuchkov <sergeykr101@gmail.com>
2018-06-07 14:58:32 +0300

    added a feature to add courses and teaching materials to cart. Changed the cart. Styles fix

--- src/app/components/login/login.component.js
+++ src/app/components/login/login.component.js
@@ -2,12 +2,14 @@ angular
   .module('app')
   .component('loginComponent', {
     templateUrl: 'app/components/login/login.tmpl.html',
-    controller: function (authService, $state) {
+    controller: function (authService, $state, localStorageService) {
       var self = this;
       this.email = '';
       this.password = '';
       this.isRemembered = false;
       this.error = '';
+      var products = {courses: {}, reports: {}, teaching_materials: {}};
+      localStorageService.set('products', products);
 
       this.login = function () {
         self.error = false;
