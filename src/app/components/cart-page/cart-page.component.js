376baba05fe183f6742407e0752298655652bbca
Sergey Kryuchkov <sergeykr101@gmail.com>
2018-06-08 17:49:37 +0300

    mearge edit-cart-page

--- src/app/components/cart-page/cart-page.component.js
+++ src/app/components/cart-page/cart-page.component.js
@@ -19,7 +19,7 @@ angular
             vm.wayBackName = 'Color Teaching Materials';
             break;
           default:
-            vm.wayBackName = 'Member Profile';
+            vm.wayBackName = 'Profile';
         }
 
         vm.products = [];
