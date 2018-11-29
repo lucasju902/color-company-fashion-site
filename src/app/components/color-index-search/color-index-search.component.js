74fafb9722ad4621a3faa913cdf4587993c07039
Sergey <sergeykr101@gmail.com>
2018-11-29 15:39:08 +0300

    created search full rgb|parser myperfectcolor sile

--- src/app/components/color-index-search/color-index-search.component.js
+++ src/app/components/color-index-search/color-index-search.component.js
@@ -8,37 +8,29 @@ angular
 
         this.colorSearch = function () {
             if (dataValidate.validate(vm.data)) {
-                // console.log("this.data", vm.data);
                 let colorWithNum = parseInt(vm.data.color.replace(/[^0-9\.]/g, ''), 10);
-                // console.log("colorWithNum", colorWithNum)
+                // console.log("colorWithNum", colorWithNum);
                 // console.log("colorWithNum", isNaN(colorWithNum))
                 // console.log("vm.data.color", vm.data.color)
                 // console.log("colorWithNum", colorWithNum.toString().length );
-
-
                 if (isNaN(!colorWithNum) || colorWithNum) {
-                    // console.log("vm.data.color", vm.data.color)
-                    vm.data = {hex: vm.data.color};
-                    // console.log("if (hexColor.toString().length === 6) vm.data", vm.data);
-                } else if (colorWithNum.toString().length === 9) {
-
+                    var regex = /[\d|,|.| |e|E|\+]+/g;
+                    vm.data.color = vm.data.color.match(regex);
+                    // console.log("matches", vm.data.color);
+                    vm.data = {rgb: vm.data.color};
+                    // console.log("vm.data", vm.data);
                 } else {
-                    delete vm.data['hex']
-                    // console.log("elseelseelseelse", vm.data);
+                    delete vm.data['rgb'];
                 }
                 $http.get(appConfig.dashboardServiceUrl + 'colors/find.json', {
                     params: vm.data
                 }).then(function (res) {
                     if (res && res.data) {
                         vm.colorData = res.data.data.map(function (item) {
-                            item = item.data.hex;
-                            return item;
+                            return item.data;
                         });
-                        // console.log("pageData", vm.colorData);
+                        // console.log("colorDatacolorDatacolorData", vm.colorData);
                     }
-                    // if (res.status === 200) {
-                    //     $state.go('thank-you');
-                    // }
                 });
             }
         };
@@ -52,7 +44,5 @@ angular
             //         console.log('err',err);
             //     }
             // });
-
-        // console.log(vm);
     }
   });
