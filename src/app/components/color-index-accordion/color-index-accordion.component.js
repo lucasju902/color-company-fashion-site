6190a49fa7c0b00cb9ffe7acfed3e373c2421def
Sergey <sergeykr101@gmail.com>
2018-12-04 11:12:08 +0300

    deploy

--- src/app/components/color-index-accordion/color-index-accordion.component.js
+++ src/app/components/color-index-accordion/color-index-accordion.component.js
@@ -76,7 +76,77 @@ angular
 //             });
 //
 //         // });
+        var text_string = "il next month when you get to James Lemon and then you’re gonna be talkin’ about t do that. And two: you dropped a hundred and fifty grand on a fuckin’ education you coulda got for a dollar fifty in late charges at the public library.";
 
+        drawWordCloud(text_string);
+
+        function drawWordCloud(text_string){
+            var common = "poop,i,me,my,myself,we,us,our,ours,ourselves,you,your,yours,yourself,yourselves,,says,said,shall";
+
+            var word_count = {};
+
+            var words = text_string.split(/[ '\-\(\)\*":;\[\]|{},.!?]+/);
+            if (words.length == 1){
+                word_count[words[0]] = 1;
+            } else {
+                words.forEach(function(word){
+                    var word = word.toLowerCase();
+                    if (word != "" && common.indexOf(word)==-1 && word.length>1){
+                        if (word_count[word]){
+                            word_count[word]++;
+                        } else {
+                            word_count[word] = 1;
+                        }
+                    }
+                })
+            }
+
+            var svg_location = "#chart";
+            var width = $(document).width();
+            var height = $(document).height();
+
+            var fill = d3.scale.category20();
+
+            var word_entries = d3.entries(word_count);
+
+            var xScale = d3.scale.linear()
+                .domain([0, d3.max(word_entries, function(d) {
+                    return d.value;
+                })
+                ])
+                .range([10,100]);
+
+            d3.layout.cloud().size([width, height])
+                .timeInterval(20)
+                .words(word_entries)
+                .fontSize(function(d) { return xScale(+d.value); })
+                .text(function(d) { return d.key; })
+                .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                .font("Impact")
+                .on("end", draw)
+                .start();
+
+            function draw(words) {
+                d3.select(svg_location).append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .append("g")
+                    .attr("transform", "translate(" + [width >> 1, height >> 1] + ")")
+                    .selectAll("text")
+                    .data(words)
+                    .enter().append("text")
+                    .style("font-size", function(d) { return xScale(d.value) + "px"; })
+                    .style("font-family", "Impact")
+                    .style("fill", function(d, i) { return fill(i); })
+                    .attr("text-anchor", "middle")
+                    .attr("transform", function(d) {
+                        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                    })
+                    .text(function(d) { return d.key; });
+            }
+
+            d3.layout.cloud().stop();
+        }
         //
         // var yellow1 = "FFFF99";
         // var yellow2 = "FFFF00";
