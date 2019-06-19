91c1618f2ca577c0feb47aadd16ca7bd9b6be463
Miroljub Markovic <forever@forevers-iMac.local>
2019-06-19 22:17:37 +0800

    added one infograph in see color tab, wip - show dialog, wip - show all infograph

--- src/app/components/dashboards/fashion/content/contentController.js
+++ src/app/components/dashboards/fashion/content/contentController.js
@@ -1,13 +1,130 @@
-angular.module('app').controller('contentFashionController',
-  ['$scope', function (scope) {
-    // Behaviour
+angular.module('app').controller('contentFashionController', [
+  '$scope', 'appConfig', 'dashboardRepository', 'charts', '$q', '$interpolate', 'repo.meta', 'common', '$http',
+  function (scope, appConfig, dashboardRepository, charts, $q, $interpolate, meta, common, $http) {
+    var vm = this;
+
+    vm.meta = {};
+    vm.filter = {};
+    vm.grayList = {};
+
+    scope.getModel = function(index) {
+      return vm.models[index];
+    }
+
+    scope.getTitle = function(index) {
+      return vm.titles[index];
+    }
+
+    scope.getDescription = function(index) {
+      return vm.descriptions[index];
+    }
+
+    scope.graphIndex = 0;
+    scope.showGraph = false;
+    scope.showGraphDialog = function(index) {
+      scope.graphIndex = index;
+      scope.showGraph = true;
+    }
+
+    scope.isShowGraphDialog = function(index) {
+      return scope.graphIndex == index && scope.showGraph;
+    }
+
+    scope.closeGraphDialog = function() {
+      scope.showGraph = false;
+    }
+
+    var cache = {
+      designers: [],
+      categories: [],
+      regions: {},
+      cities: []
+    };
+
+    var loading = {
+      ready: false,
+      designersReady: $q.defer(),
+      metaLoaded: $q.defer(),
+      metaReady: $q.defer()
+    };
+
+    function getObject(id, prop) {
+      var obj = {
+        id: "all",
+        title: "all"
+      };
+
+      if (id == "") {
+        return obj;
+      }
+
+      for (var i = 0; i < scope.controlsData[prop].length; i++) {
+        var data = scope.controlsData[prop][i];
+        if (data.id == id) {
+          obj = data;
+          break;
+        }
+      }
+      return obj;
+    }
+
+    vm.prepareRequestParams = function () {
+      var params = {
+        city: getObject(scope.menus.city, 'cities').title,
+        year: scope.menus.year == "" ? "all" : scope.menus.year,
+        season: getObject(scope.menus.season, 'seasons').title,
+        category: getObject(scope.menus.category, 'categories').title,
+        region: scope.menus.region == "" ? "all" : scope.menus.region,
+        designer: getObject(scope.menus.designer, 'designers').title
+      };
+
+      return params;
+    };
+
+    vm.prepareColorsParams = function () {
+      var param = {};
+      if (scope.menus.city != "") {
+        param.city_id = scope.menus.city;
+      }
+      if (scope.menus.season != "") {
+        param.season_id = scope.menus.season;
+      }
+      if (scope.menus.category != "") {
+        param.category_id = scope.menus.category;
+      }
+      if (scope.menus.designer != "") {
+        param.designer_id = scope.menus.designer;
+      }
+
+      if (scope.menus.year == "") {
+        param.year_id = "all";
+      } else {
+        param.year_id = scope.menus.year;
+      }
+
+      return param;
+    };
+
+    vm.prepareColors = function () {
+      if (scope.menus.season != "") {
+        return {all: getObject(scope.menus.season, 'seasons'), category: 'season'};
+      }
+      if (scope.menus.city != "") {
+        return {all: getObject(scope.menus.city, 'cities'), category: 'city'};
+      }
+      if (scope.menus.category != "") {
+        return {all: getObject(scope.menus.category, 'categories'), category: 'category'};
+      }
+      if (scope.menus.designer != "") {
+        return {all: getObject(scope.menus.designer, 'desingers'), category: 'designer'};
+      }
+      return {all: {id: 2018}, category: 'year'};
+    };
 
     scope.tab = 1;
     scope.setTab = function (tabId) {
       scope.tab = tabId;
     };
-
-    console.log('------------------------');
 
     scope.isSet = function (tabId) {
         return scope.tab === tabId;
@@ -42,4 +159,188 @@ angular.module('app').controller('contentFashionController',
     scope.toggleDesignerImagesExpandedMode = function () {
       scope.designerImagesExpanded = !scope.designerImagesExpanded;
     };
-  }]);
+
+    vm.charts = [];
+    scope.$watch('menus', function() {
+      vm.charts = [{
+        qNumber: 'CO1a',
+        id: 'colorsByCityPeriod',
+        group: 'colorsByCityPeriod',
+        title: 'Color Popularity Overview',
+        chartTitle: 'Color Popularity Overview',
+        api: function () {
+          return charts.colorGroupsByCityPeriod(vm.prepareRequestParams())
+            .then(function (results) {
+              return results;
+            });
+        },
+        filters: {
+          category: true,
+          season: true,
+          year: true,
+          region: true,
+          city: true
+        },
+        titleGroups: [
+          ['category', 'season', 'year'],
+          ['region']
+        ],
+        options: {
+          extraView: true
+        }
+      },
+      {
+        qNumber: 'CO1b',
+        id: 'colorsByCityPeriod1',
+        group: 'colorsByCityPeriod1',
+        title: 'Expanded Color Popularity Overview',
+        chartTitle: 'Expanded Color Popularity Overview {{vm.parseTitle(0)}} {{vm.parseTitle(1)}}',
+        api: function () {
+          return $q(function (resolve) {
+            charts.colorGroupsByCityPeriod(vm.prepareRequestParams())
+              .then(function (results) {
+                var param = vm.prepareColors();
+                dashboardRepository[param.category].getColorPalette(param.all.id, vm.prepareColorsParams(), 250)
+                  .then(function (data) {
+                    _.each(results, function (colorGroup) {
+                      colorGroup.colors = [];
+                      data.forEach(function (t) {
+                        if (colorGroup.title.toLowerCase() === t.color.family.toLowerCase() && colorGroup.colors.length < 17) {
+                          colorGroup.colors.push(t.color.color.hex);
+                        }
+                      });
+                      resolve(results);
+                    });
+                  });
+              });
+          });
+        },
+        filters: {
+          category: true,
+          region: true,
+          city: true,
+          season: true,
+          year: true
+        },
+        titleGroups: [
+          ['category', 'season', 'year'],
+          ['region']
+        ],
+        options: {
+          extraView: true
+        }
+      }];
+    });
+
+    vm.models = [];
+    vm.titles = [];
+    vm.descriptions = [];
+
+    // Behaviour
+    meta.objects().then(function (result) {
+      var years = [];
+      for (var i = result.years.to; i >= result.years.from; i--) {
+        years.push({id: i, title: i});
+      }
+
+      vm.meta.years = years;
+      vm.meta.colorGroups = result.colorGroups;
+      vm.meta.categories = result.categories;
+      vm.meta.seasons = result.seasons;
+      vm.meta.regions = common.generic.regions;
+      vm.meta.cities = result.cities;
+
+      _.each(vm.meta, function (item, key) {
+        var newTitle = 'All ' + key;
+        if (key !== 'colorGroups') {
+          item.unshift({id: 'all', title: newTitle.toUpperCase(), region: 'all', serverName: 'all', all: true})
+        }
+      });
+
+      angular.copy(vm.meta.cities, cache.cities);
+
+      // vm.filter.color = vm.meta.colorGroups[0];
+      // // vm.filter.year = _.find(vm.meta.years, {id: 2017}) || vm.meta.years[vm.meta.years.length - 1];
+      // vm.filter.year = vm.meta.years[0];
+      // vm.filter.season = vm.meta.seasons[0];
+      // vm.filter.category = vm.meta.categories[0];
+      // vm.filter.city = vm.meta.cities[0];
+      // vm.filter.region = vm.meta.regions[0];
+
+      // loading.metaLoaded.resolve();
+
+      vm.charts.forEach(function(currentChart, i) {
+        vm.models[i] = null;
+        vm.titles[i] = '';
+        vm.descriptions[i] = '';
+
+        currentChart.api().then(function(result) {
+          vm.models[i] = result;
+          if (vm.charts[i].apiAfter) {
+            vm.charts[i].apiAfter(vm.models[i], result);
+          }
+          vm.titles[i] = prepareTitle(vm.charts[i].chartTitle);
+          prepareDescription(i);
+        });
+      });
+  
+    });
+
+    function prepareTitle(title) {
+      var exp = $interpolate(title);
+      return exp(scope);
+    }
+
+    function prepareDescription(i) {
+      vm.descriptions[i] = vm.descriptions[i] || (scope.menus.year + ' | COLORS-' + vm.charts[i].qNumber + ' | CITIES-' +
+      scope.menus.city + ' | REGIONS-' + scope.menus.region + ' | DESIGNER-' + scope.menus.designer +
+        ' | SEASONS-' + scope.menus.season);
+
+      var regionId = null;
+      switch (scope.menus.region) {
+        case 'europe':
+          regionId = 2;
+          break;
+        case 'north_america':
+          regionId = 3;
+          break;
+        case 'latin_america':
+          regionId = 4;
+          break;
+        case 'asia_pacific':
+          regionId = 1;
+          break;
+      }
+
+      var yearFrom = null;
+      var yearTo = scope.menus.year === 'all' ? vm.meta.years[1].title : scope.menus.year;
+      if (vm.charts[i].qNumber === 'CO3a' || vm.charts[i].qNumber === 'SE2a' || vm.charts[i].qNumber === 'SE2b') {
+        yearFrom = yearTo - 4;
+      } else if (vm.charts[i].qNumber === 'CA3a' || vm.charts[i].qNumber === 'CA3b') {
+        yearFrom = yearTo - 2;
+      } else if (vm.charts[i].qNumber === 'DE2a' || vm.charts[i].qNumber === 'DE2b') {
+        yearFrom = yearTo - 1;
+      }
+
+      $http({
+        url: (appConfig.webServiceUrl + 'stats'),
+        method: 'GET',
+        params: {
+          fashionSeason: scope.menus.season === 'all' ? null : scope.menus.season,
+          fashionDesigner: scope.menus.designer === 'all' ? null : scope.menus.designer,
+          fashionRegion: regionId || null,
+          fashionCity: scope.menus.city === 'all' ? null : scope.menus.city,
+          fashionCategory: scope.menus.category === 'all' ? null : scope.menus.category,
+          fashionYear: yearFrom || scope.menus.year === 'all' ? null : scope.menus.year,
+          yearFrom: yearFrom || null,
+          yearTo: yearFrom ? yearTo : null
+        }
+      }).then(function (res) {
+        // vm.grayList = res.data.data;
+        vm.descriptions[i] = 'YEARS-' + res.data.counts.years + ' | COLORS-' + res.data.counts.colors +
+          ' | CITIES-' + res.data.counts.cities + ' | REGIONS-' + res.data.counts.regions +
+          ' | DESIGNER-' + res.data.counts.designers + ' | SEASONS-' + res.data.counts.seasons;
+      });
+    }
+  }
+]);
